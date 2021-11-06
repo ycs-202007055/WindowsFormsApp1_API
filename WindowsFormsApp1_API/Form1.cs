15daa4ef31a529adc6ef13cc9502b91055a7e098
이태양 <xodid4025@naver.com>
2021-11-07 00:13:32 +0900

    15

--- WindowsFormsApp1_API/Form1.cs
+++ WindowsFormsApp1_API/Form1.cs
@@ -28,7 +28,7 @@ namespace WindowsFormsApp1_API
 
             axKHOpenAPI1.OnEventConnect += OnEventConnect;
             axKHOpenAPI1.CommConnect();
-            axKHOpenAPI1.OnReceiveTrData += onReceiveTrData;//????~~~테스트14
+            axKHOpenAPI1.OnReceiveTrData += onReceiveTrData;//????~~~테스트15
             
             
         }
